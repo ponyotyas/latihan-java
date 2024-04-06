a693edcc5d0c4057b16ff6de7218912206827080
ponyotyas <dianrhy05@gmail.com>
2024-04-07 00:04:39 +0700

    Latihan Pertemuan 4

---
+++ Latihan Pertemuan 3/contohFor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan4;
+
+/**
+ *
+ * @author User
+ */
+public class contohFor {
+    public static void main(String[] args) {
+        int a;
+        for(a = 1; a <= 10; ++a){
+            System.out.println(a);
+        }
+    }
+}
+
